96a3b05512ccf0f1f09c88b1a22861121043ce6b
Helder Boone Borges <Helder.Borges@ey.com>
2022-04-12 11:18:36 -0300

    Utilizando Fixtures

---
+++ 01 - Teste de Unidade/Features.Tests/02 - Fixtures/ClienteTestsFixture.cs
@@ -0,0 +1,45 @@
+﻿using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture> { }
+
+    //Objeto preparado antes de instanciar a classe de teste e so é removido depois de rodar o ultimo metodo de teste da classe
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Helder",
+                "Borges",
+                DateTime.Now.AddYears(-30),
+                "helder@email.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now.AddYears(-30),
+                "helder@email.com",
+                true,
+                DateTime.Now);
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
