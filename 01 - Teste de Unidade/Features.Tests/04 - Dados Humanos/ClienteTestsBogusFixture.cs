1b3d51db50384c544107b11c6ac7e7c96626fa59
Helder Boone Borges <Helder.Borges@ey.com>
2022-04-20 16:55:01 -0300

    Gerando dados humanos

---
+++ 01 - Teste de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -0,0 +1,58 @@
+﻿using Bogus;
+using Bogus.DataSets;
+using Features.Clientes;
+using System;
+using Xunit;
+
+namespace Features.Tests
+{
+    [CollectionDefinition(nameof(ClienteBogusCollection))]
+    public class ClienteBogusCollection : ICollectionFixture<ClienteTestsBogusFixture> { }
+
+    //Objeto preparado antes de instanciar a classe de teste e so é removido depois de rodar o ultimo metodo de teste da classe
+    public class ClienteTestsBogusFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+            //var email = new Faker().Internet.Email("helder", "boone", "gmail");
+            //var clienteFaker = new Faker<Cliente>();
+            //clienteFaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(80, DateTime.Now.AddYears(-18)),
+                    string.Empty,
+                    true,
+                    DateTime.Now))
+                .RuleFor(c => c.Email, (f, c) =>
+                f.Internet.Email(c.Nome.ToLower(), c.SobreNome.ToLower()));
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var genero = new Faker().PickRandom<Name.Gender>();
+
+            var cliente = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    Guid.NewGuid(),
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Date.Past(1, DateTime.Now.AddYears(1)),
+                    string.Empty,
+                    false,
+                    DateTime.Now));
+
+            return cliente;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
