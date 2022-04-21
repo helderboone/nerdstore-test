36f0a1a77bdca2eb68b28982dfe837486f345e3e
Helder Boone Borges <Helder.Borges@ey.com>
2022-04-21 14:20:46 -0300

    Simulando retorno de metodos com metodo com Mock

--- 01 - Teste de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
+++ 01 - Teste de Unidade/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixture.cs
@@ -2,6 +2,9 @@
 using Bogus.DataSets;
 using Features.Clientes;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Features.Tests
@@ -14,24 +17,39 @@ namespace Features.Tests
     {
         public Cliente GerarClienteValido()
         {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> GerarClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
+        {
             var genero = new Faker().PickRandom<Name.Gender>();
             //var email = new Faker().Internet.Email("helder", "boone", "gmail");
             //var clienteFaker = new Faker<Cliente>();
             //clienteFaker.RuleFor(c => c.Nome, (f, c) => f.Name.FirstName());
 
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
                     f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     string.Empty,
-                    true,
+                    ativo,
                     DateTime.Now))
                 .RuleFor(c => c.Email, (f, c) =>
                 f.Internet.Email(c.Nome.ToLower(), c.SobreNome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInvalido()
--- 01 - Teste de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ 01 - Teste de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using Features.Clientes;
 using MediatR;
 using Moq;
+using System.Linq;
 using System.Threading;
 using Xunit;
 
@@ -34,6 +35,46 @@ namespace Features.Tests
             //O parametro cliente deve ser o mesmo passado no cliente service adicionar do contrario ele achara que é outro cenario
             clienteRepo.Verify(r => r.Adicionar(cliente), Times.Once);
             mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Once);
+        }
+
+        [Fact(DisplayName = "Adicionar Cliente com Falha")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
+        {
+            // Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInvalido();
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Assert
+            Assert.False(cliente.EhValido());
+            clienteRepo.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
+        }
+
+        [Fact(DisplayName = "Obter Clientes Ativos")]
+        [Trait("Categoria", "Cliente Service Mock Tests")]
+        public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
+        {
+            // Arrange
+            var clienteRepo = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
+
+            clienteRepo.Setup(c => c.ObterTodos()).Returns(_clienteTestsFixture.GerarClientesVariados());
+
+            var clienteService = new ClienteService(clienteRepo.Object, mediatr.Object);
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos();
+
+            // Assert
+            clienteRepo.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c => !c.Ativo) > 0);
         }
     }
 }
