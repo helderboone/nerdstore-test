65bf9a97b6eafb23b7945385b27285ff8a2ee5e7
Helder Boone Borges <Helder.Borges@ey.com>
2022-06-01 14:50:59 -0300

    Pedido - Adicionar Item Command

---
+++ 02 - TDD/src/NerdStore.Core/Messages/Command.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NerdStore.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; set; }
+
+        public abstract bool EhValido();
+    }
+}
