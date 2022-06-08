2b4f78f4542b8b2982c60d2186a9d3ac34013a15
Helder Boone Borges <Helder.Borges@ey.com>
2022-06-08 10:36:45 -0300

    Pedido - Command Handler - Parte 1

---
+++ 02 - TDD/src/NerdStore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,35 @@
+﻿using NerdStore.Core.Messages;
+using System;
+using System.Collections.Generic;
+
+namespace NerdStore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        private List<Event> _notificacoes;
+        public IReadOnlyCollection<Event> Notificacoes => _notificacoes?.AsReadOnly();
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        public void AdicionarEvento(Event evento)
+        {
+            _notificacoes = _notificacoes ?? new List<Event>();
+            _notificacoes.Add(evento);
+        }
+
+        public void RemoverEvento(Event evento)
+        {
+            _notificacoes?.Remove(evento);
+        }
+
+        public void LimparEventos()
+        {
+            _notificacoes?.Clear();
+        }
+    }
+}
--- 02 - TDD/src/NerdStore.Core/Messages/Message.cs
+++ 02 - TDD/src/NerdStore.Core/Messages/Message.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MediatR;
+using System;
 
 namespace NerdStore.Core.Messages
 {
@@ -11,5 +12,15 @@ namespace NerdStore.Core.Messages
 
         public string MessageType { get; protected set; }
         public Guid AggregateId { get; protected set; }
+    }
+
+    public abstract class Event : Message, INotification
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Event()
+        {
+            Timestamp = DateTime.Now;
+        }
     }
 }
