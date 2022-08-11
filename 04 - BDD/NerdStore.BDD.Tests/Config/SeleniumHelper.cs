55cc54fd3562e14f9d0e51c4bd88d13e73acf503
Helder Boone Borges <Helder.Borges@ey.com>
2022-08-11 17:04:01 -0300

    Apresentando o Selenium

---
+++ 04 - BDD/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -0,0 +1,22 @@
+﻿namespace NerdStore.BDD.Tests.Config
+{
+    public class SeleniumHelper
+    {
+    }
+
+    public class ConfigurationHelper
+    {
+
+    }
+
+    public enum Browser
+    {
+        Chrome,
+        Firefox
+    }
+
+    public static class WebDriverFactory
+    {
+
+    }
+}
