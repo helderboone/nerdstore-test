4486b8857d8704d24599f729de1b17f92ca7f43e
Helder Boone Borges <Helder.Borges@ey.com>
2022-08-11 17:43:14 -0300

    Configurando o Selenium da forma certa

--- 04 - BDD/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
+++ 04 - BDD/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -1,12 +1,59 @@
-﻿namespace NerdStore.BDD.Tests.Config
+﻿using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExptectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace NerdStore.BDD.Tests.Config
 {
-    public class SeleniumHelper
+    public class SeleniumHelper : IDisposable
     {
+        public IWebDriver WebDriver;
+        public readonly ConfigurationHelper Configuration;
+        public WebDriverWait Wait;
+
+        public SeleniumHelper(Browser browser, ConfigurationHelper configuration, bool headless = true)
+        {
+            Configuration = configuration;
+            WebDriver = WebDriverFactory.CreateWebDriver(browser, Configuration.WebDrivers, headless);
+            WebDriver.Manage().Window.Maximize();
+            Wait = new WebDriverWait(WebDriver, TimeSpan.FromSeconds(30));
+        }
+
+        public string ObterUrl()
+        {
+            return WebDriver.Url;
+        }
+
+        public void IrParaUrl(string url)
+        {
+            WebDriver.Navigate().GoToUrl(url);
+        }
+
+        public void ClicarLinkPorTexto(string linkText)
+        {
+            Wait.Until(ExptectedConditions.ElementIsVisible(By.LinkText(linkText))).Click();
+        }
+
+        public void Dispose()
+        {
+        }
     }
 
     public class ConfigurationHelper
     {
+        private readonly IConfiguration _config;
 
+        public ConfigurationHelper()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+
+        public string WebDrivers => $"{_config.GetSection("WebDrivers").Value}";
     }
 
     public enum Browser
@@ -17,6 +64,29 @@
 
     public static class WebDriverFactory
     {
+        public static IWebDriver CreateWebDriver(Browser browser, string caminhoDriver, bool headless)
+        {
+            IWebDriver webDriver = null;
 
+            switch (browser)
+            {
+                case Browser.Firefox:
+                    var optionsFireFox = new FirefoxOptions();
+                    if (headless)
+                        optionsFireFox.AddArgument("--headless");
+
+                    webDriver = new FirefoxDriver(caminhoDriver, optionsFireFox);
+                    break;
+                case Browser.Chrome:
+                    var options = new ChromeOptions();
+                    if (headless)
+                        options.AddArgument("--headless");
+
+                    webDriver = new ChromeDriver(caminhoDriver, options);
+                    break;
+            }
+
+            return webDriver;
+        }
     }
 }
